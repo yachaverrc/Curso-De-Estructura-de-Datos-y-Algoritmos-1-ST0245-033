1839dee429e94741410386224589970cbd14ca60
yachaverrc <60229713+yachaverrc@users.noreply.github.com>
2020-05-12 10:05:18 -0500

    Taller 10 listo

---
+++ talleres/taller10/Node.java
@@ -0,0 +1,11 @@
+public class Node {
+
+    public Node left,right;
+    public int data;
+
+    public Node(int data) {
+        this.left = null;
+        this.right = null;
+        this.data = data;
+    }
+}
